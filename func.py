02073d3f111fdfffd22cf670184adc3b409d1a56
Arthur Almeida <almeidaarthur128@gmail.com>
2025-05-28 18:26:31 -0300

    Iniciando implementação Redis

--- func.py
+++ func.py
@@ -1,9 +1,13 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import datetime
+from redis import Redis
+
+# Adicioanando Redis
+redis_client = Redis(host='localhost', port=6379, db=0)
 
 # Criação de uma nova tarefa
-def adicionar_tarefa(colecao, titulo, descricao, tags):
+def adicionar_tarefa(colecao, titulo, descricao, tags,redis_client):
     nova_tarefa = {
         "titulo": titulo,
         "descricao": descricao,
@@ -13,6 +17,9 @@ def adicionar_tarefa(colecao, titulo, descricao, tags):
         "comentarios": []
     }
     colecao.insert_one(nova_tarefa)
+    # A cada nova tarefa criada, incrementa o contador no Redis
+    if redis_client:
+        redis_client.hincrby('tarefas', 'Criadas', 1)  
 
 # Busca de tarefas com filtros opcionais
 def listar_tarefas(colecao, status=None, tags=None):
@@ -24,12 +31,24 @@ def listar_tarefas(colecao, status=None, tags=None):
     return list(colecao.find(filtros))
 
 # Atualização de uma tarefa existente
-def modificar_tarefa(colecao, id_tarefa, dados_novos):
+def modificar_tarefa(colecao, id_tarefa, dados_novos,redis_client):
+    #Armazena atualização no banco
+    result = colecao.update_one({"_id": ObjectId(id_tarefa)}, {"$set": dados_novos})
     colecao.update_one({"_id": ObjectId(id_tarefa)}, {"$set": dados_novos})
+    # Incrementa o contador de modificações no Redis
+    if redis_client and result.modified_count > 0:
+        redis_client.hincrby('tarefas', 'modificadas', 1)
 
 # Remoção de uma tarefa do banco
-def remover_tarefa(colecao, id_tarefa):
+def remover_tarefa(colecao, id_tarefa,redis_client):
+    #Armazena remoção no banco
+    result = colecao.delete_one({"_id": ObjectId(id_tarefa)})
     colecao.delete_one({"_id": ObjectId(id_tarefa)})
+    # Incrementa o contador de remoções no Redis
+    if redis_client and result.deleted_count > 0:
+        redis_client.hincrby('tarefas', 'removidas', 1)
+
+
 
 # Inserção de comentário em uma tarefa específica
 def inserir_comentario(colecao, id_tarefa, texto_comentario):
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from pymongo import MongoClient
 from func import *
 
 # Conexão com MongoDB Atlas
-cliente = MongoClient("mongodb+srv://lucasnascimento:lusak2025@cluster0.aptlpuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+cliente = MongoClient("mongodb+srv://ArthurSouza:210913@cluster0.dqda2cv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 banco = cliente.get_database("GerenciadorTarefas")
 tarefas = banco.get_collection("ListaTarefas")
+
 
 while True:
     print("\n" + "-"*40)
@@ -15,6 +16,7 @@ while True:
     print("4. Remover tarefa")
     print("5. Inserir comentário")
     print("6. Sair")
+    print("7. Ver métricas (Redis):")
     opcao = input("Opção: ")
 
     if opcao == "1":
@@ -22,7 +24,7 @@ while True:
         titulo = input("Título da tarefa: ")
         descricao = input("Descrição da tarefa: ")
         tags = input("Tags (separadas por vírgula): ").split(",")
-        adicionar_tarefa(tarefas, titulo, descricao, tags)
+        adicionar_tarefa(tarefas, titulo, descricao, tags,redis_client)
         print("Tarefa adicionada com sucesso!")
 
     elif opcao == "2": 
@@ -47,13 +49,13 @@ while True:
         novo_status = input("Novo status (pendente/concluída): ")
         nova_descricao = input("Nova descrição: ")
         tags = input("Novas tags (separadas por vírgula): ").split(",")
-        modificar_tarefa(tarefas, id_tarefa, {"status": novo_status, "descricao": nova_descricao, "tags": tags})
+        modificar_tarefa(tarefas, id_tarefa, {"status": novo_status, "descricao": nova_descricao, "tags": tags},redis_client)
         print("Tarefa modificada com sucesso!")
 
     elif opcao == "4":
         print("\n" + "-"*40)
         id_tarefa = input("ID da tarefa a remover: ")
-        remover_tarefa(tarefas, id_tarefa)
+        remover_tarefa(tarefas, id_tarefa,redis_client)
         print("Tarefa removida com sucesso!")
 
     elif opcao == "5":
@@ -62,6 +64,13 @@ while True:
         texto_comentario = input("Texto do comentário: ")
         inserir_comentario(tarefas, id_tarefa, texto_comentario)
         print("Comentário inserido com sucesso!")
+
+    elif opcao == "7":
+        metricas = redis_client.hgetall("task:metrics")
+        print("\nMétricas:")
+        for chave, valor in metricas.items():
+            print(f"{chave.decode()}: {int(valor)}")
+
 
     elif opcao == "6":
         print("Saindo...")
